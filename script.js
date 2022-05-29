20223f007bcb4daef2d75a9ba07e3dd55a3f68bb
figtreevic <figtreevic@gmail.com>
2022-05-29 17:29:12 -0300

    First upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelectorAll('.faq-toggle');
+
+toggle.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle
+        ('active')
+    })
+})
